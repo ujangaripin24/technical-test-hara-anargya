ebcf55bc11bafaff48741679948a44f6386eee19
ujangarifin06@gmail.com <ujangarifin06@gmail.com>
2024-02-13 18:19:27 +0700

    fix dialog with transition

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -105,7 +105,6 @@ export default {
           </a>
         </div>
       </div>
-
     </div>
     <div class="mt-4">
       <div class="relative overflow-x-auto shadow sm:rounded-lg">
@@ -162,16 +161,17 @@ export default {
       </div>
     </div>
     <!-- Main modal -->
-    <transition name="fade" appear>
-      <div v-show="showModal" @click.self="clickHideModal" tabindex="-1" aria-hidden="true"
-        class="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative p-4 w-90 max-w-2xl max-h-full">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+    <transition name="fade">
+      <div v-show="showModal" class="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-60">
+        <div @click.self="clickHideModal" tabindex="-1" aria-hidden="true"
+          class="relative p-4 w-full max-w-2xl max-h-full">
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700" style="backdrop-filter: blur(10px);">
             <div class="p-4 md:p-5 space-y-4">
               <div class="flex justify-end">
                 <button @click="clickHideModal" type="button"
                   class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
-                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
                     <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                   </svg>
@@ -208,4 +208,5 @@ export default {
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
-}</style>
+}
+</style>
